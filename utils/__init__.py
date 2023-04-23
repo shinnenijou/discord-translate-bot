d4080b7d483ba7c03d80d30e61e118d4f27117cc
shinnenijou <littlesword111@gmail.com>
2023-04-23 13:24:27 +0800

    update

--- utils/__init__.py
+++ utils/__init__.py
@@ -42,8 +42,21 @@ def slice_text(text: str, max_length: int = 20, prefix: str = '【', suffix: str
 def sync(coroutine):
     return asyncio.get_event_loop().run_until_complete(coroutine)
 
-def replace(text: str) -> str:
-    text.replace('メディア', 'ミリヤ')
-    text.replace('ミディア', 'ミリヤ')
+class TextProcessor:
+    def __init__(self):
+        self.__words = {}
+        self.load_words()
 
-    return text
+    def load_words(self):
+        self.__words = {}
+        from dictionary import REPLACE_MAP
+        for _old, _new in REPLACE_MAP.items():
+            self.__words[_old] = _new
+
+    def deal(self, text: str) -> str:
+        for _old, _new in self.__words.items():
+            text = text.replace(_old, _new)
+
+        return text
+
+test_processor = TextProcessor()
