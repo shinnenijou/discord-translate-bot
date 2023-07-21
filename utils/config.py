878e73852c25122da09f0f28d54d04dee0ffe11c
shinnenijou <littlesword111@gmail.com>
2023-07-21 18:18:19 +0800

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -18,7 +18,8 @@ class Config:
         'api': '',
         'send': '',  # 'live', 'webhook'
         'webhook_url': '',
-        'send_lag': ''
+        'send_lag': '',
+        'language': '',
     }
 
     # 运行基本功能所必须的字段, 如果没有配置这些字段, 则应当拒绝启动
